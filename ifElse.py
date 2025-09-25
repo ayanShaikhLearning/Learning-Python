c7a25d88fee769472b0f4db1b53cbea4afc7479e
ayanShaikhLearning <econeliteclassroom@gmail.com>
2025-09-25 18:09:56 +0530

    operetors and conditionals

---
+++ ifElse.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("Minor")
+elif age < 60:
+    print("Adult")
+else:
+    print("Senior Citizen")
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello World")
-print("This will be commited")
+# Numbers, Strings, Booleans
+a = 5
+b = 2.5
+c = "Python"
+d = True
+
+print(type(a), type(b), type(c), type(d))
+
+# Type casting
+num = int("100")
+flt = float("12.5")
+print(num + flt)
